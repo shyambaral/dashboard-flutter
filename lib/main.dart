589178055601567c5ef76fe05e385b82af7e9c8f
shyambaral <shyam.baral12345@gmail.com>
2022-07-08 18:35:54 +0545

    first

--- lib/main.dart
+++ lib/main.dart
@@ -52,6 +52,7 @@ class Dashboard extends StatelessWidget {
                   Image.asset("images/ram.jpg",width: 52.0,)
                   Image.asset("images/ram.jpg",width: 52.0,)
                   Image.asset("images/ram.jpg",width: 52.0,)
+                  Image.asset("images/ram.jpg",width: 52.0,)
                   
                 ],
               ),
